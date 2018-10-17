720e98457832e8de0ae2a131f2bb90b10531b693
wangjixiao <642907599@qq.com>
2018-10-17 12:53:43 +0800

    添加代码

--- updateSet.sh
+++ updateSet.sh
@@ -5,9 +5,9 @@ project_name=WJXAlertView
 #工程绝对路径
 project_path=$(dirname $0)
 #project工程文件路径
-folder_ProjectPath=${project_path}/${project_name}/@{project_name}.xcodeproj/project.pbxproj
+folder_ProjectPath=${project_path}/${project_name}/${project_name}.xcodeproj/project.pbxproj
 #infor文件路径
-folder_InfoPath=${project_path}/${project_name}/Supporting Files/Info.plist
+folder_InfoPath=${project_path}/${project_name}/${project_name}/Supporting\ Files/info.plist
 
 
 #============== 选择模式 ========================#
@@ -44,22 +44,25 @@ echo "上传的构建号："
 read buildCode
 done
 
+echo ${folder_InfoPath}
+
 /usr/libexec/PlistBuddy -c "Set CFBundleShortVersionString ${versionCode}" ${folder_InfoPath}
 /usr/libexec/PlistBuddy -c "Set CFBundleVersion ${buildCode}" ${folder_InfoPath}
 
 ##修改参数
-if [ $APPNumber == 1 ] ;then
+if [ $APPNumber == 2 ] ;then
+   #马甲包
    sed -i -r 's/ASSETCATALOG_COMPILER_APPICON_NAME.*$/ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon-1;/g' ${folder_ProjectPath}
    sed -i -r 's/ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME.*$/ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME = LaunchImage-1;/g' ${folder_ProjectPath}
 else
-   ##Q房网
+   ##正式包
    sed -i -r 's/ASSETCATALOG_COMPILER_APPICON_NAME.*$/ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon;/g' ${folder_ProjectPath}
    sed -i -r 's/ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME.*$/ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME = LaunchImage;/g' ${folder_ProjectPath}
 fi
-if [[ -d ${folder_InfoPath}-r ]]; then
+if [[ -f ${folder_InfoPath}-r ]]; then
    rm -rf ${folder_InfoPath}-r
 fi
-if [[ -d ${folder_ProjectPath}-r ]]; then
+if [[ -f ${folder_ProjectPath}-r ]]; then
    rm -rf ${folder_ProjectPath}-r
 fi
 
